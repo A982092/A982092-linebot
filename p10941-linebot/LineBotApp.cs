e6bc3b2c561a060c34f266524def1eb550a3b8d7
ayun <10330517@ms.sljh.tc.edu.tw>
2023-05-30 16:37:05 +0800

    新增貓

--- p10941-linebot/LineBotApp.cs
+++ p10941-linebot/LineBotApp.cs
@@ -1,4 +1,6 @@
-﻿using Line.Messaging;
+﻿using System.Net.Mime;
+using System.Reflection.PortableExecutable;
+using Line.Messaging;
 using Line.Messaging.Webhooks;
 
 namespace p10941_linebot;
@@ -6,6 +8,8 @@ namespace p10941_linebot;
 public class LineBotApp : WebhookApplication
 {
     private readonly LineMessagingClient _messagingClient;
+
+    private static Dictionary<string, string> _pool=new Dictionary<string, string>();
     public LineBotApp(LineMessagingClient lineMessagingClient)
     {
         _messagingClient = lineMessagingClient;
@@ -25,6 +29,24 @@ public class LineBotApp : WebhookApplication
                 //使用者Id
                 var userId = ev.Source.UserId;
                     
+                var text =((TextEventMessage)ev.Message).Text;
+
+                if (PoolHasMsg(text))
+                {
+                    string response = GetResponse(text);
+                    result = new List<ISendMessage>
+                    {
+                        new TextMessage(response)
+                    };
+                }
+                else
+                {
+                    if (CheckFormat(text))
+                    {
+                        TeachDog(text);
+                    }
+                }
+                
                 //回傳 hellow
                 result = new List<ISendMessage>
                 {
@@ -36,5 +58,62 @@ public class LineBotApp : WebhookApplication
 
         if (result != null)
             await _messagingClient.ReplyMessageAsync(ev.ReplyToken, result);
+    }
+
+    private bool PoolHasMsg(string inputMsg)
+    {
+        return _pool.ContainsKey(inputMsg);
+    }
+
+    private string GetResponse(string inputMsg)
+    {
+        return _pool[inputMsg];
+        return "";
+    }
+    private bool CheckFormat(string inputMsg)
+    {
+        bool result = false;
+        try
+        {
+            string[] subs = inputMsg.Split(';');
+            if (subs.Length == 3)
+            {
+                if (subs[0] == "貓")
+                {
+                    return true;
+                }
+            }
+        }
+        catch (Exception exp)
+        {
+            Console.WriteLine(exp);
+            throw;
+        }
+        
+        
+        return result;
+    }
+
+    private void TeachDog(string inputMsg)
+    {
+        try
+        {
+            string[] subs = inputMsg.Split(';');
+            if (subs.Length == 3)
+            {
+                if (subs[0] == "貓")
+                {
+                    _pool.Add(subs[1],subs[2]);
+                }
+            }
+        }
+        catch (Exception exp)
+        {
+            Console.WriteLine(exp);
+            throw;
+        }
+        
+        
+        
     }
 }
